2fd8df3131df515df72c813255693aefb5d30463
6keeper <82912166+6keeper@users.noreply.github.com>
2021-08-13 02:14:44 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Homework7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] myNewArray = new int[3, 3]
+            {
+                {4,28,80},
+                {32,12,36},
+                {55,67,98 }
+            };
+            int rowsNumber = myNewArray.GetUpperBound(0)+1;
+            Console.WriteLine();
+            }
+        }
+    }
+}
