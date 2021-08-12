cd05dbb7f504dc04fe226cac7afb98bd71923ab2
6keeper <82912166+6keeper@users.noreply.github.com>
2021-08-13 02:02:49 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Homework7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[6];
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.WriteLine($"Insert {i} array element");
+                myArray[i] = int.Parse(Console.ReadLine());
+            }
+            Array.Sort(myArray);
+            Array.Reverse(myArray);
+            foreach (var element in myArray)
+            {
+                Console.Write(element + " ");
+            }
+        }
+    }
+}
